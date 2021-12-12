5efc71407832b7cb635a1c8bcc6b6073704ea3dc
Ashutosh Singhai <ashutosh.empli5@gmail.com>
2021-12-12 12:35:43 +0530

    added express inside the api file

--- functions/api.js
+++ functions/api.js
@@ -1,3 +1,38 @@
-const app = require("../index");
+import express from "express";
+const app = express();
+const router = express.Router();
+import fetch from "node-fetch";
+
+app.use("/.netlify/functions/api", router);
+
+router.get("/", (req, res) => {
+  res.json({ name: "Ashutosh" });
+});
+
+router.get("/suggestions", async (req, res) => {
+  const { search, size = 10, page = 1 } = req.query;
+  const result = await fetch(
+    `https://pricee.com/api/v1/suggest.php?q=${search}&lang=en`
+  ).then((e) => e.json());
+  res.json(result.data || []);
+});
+
+router.get("/prices", async (req, res) => {
+    const { search, size = 10, page = 1 } = req.query;
+    const result = await fetch(
+      `https://pricee.com/api/v1/search.php?q=${search}&size=${size}&page=${page}&lang=en&vuid=0&platform=1`
+    ).then((e) => e.json());
+    res.json(
+      result.data.map((e) => {
+        return {
+          title: e.title,
+          name: e.source_display_name,
+          price: e.source_display_name,
+          image: e.image,
+        };
+      })
+    );
+  });
+
 const serverless = require("serverless-http");
-exports.handler = serverless(app)
+exports.handler = serverless(app);
